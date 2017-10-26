162f43bd41c1ba4f06a36e6a8d7a238d41721e22
hitler11319 <hitler11319@gmail.com>
2017-10-26 08:36:02 +0800

    use python to write tcp client and server connection

--- b10409006_client.py
+++ b10409006_client.py
@@ -1,12 +1,17 @@
 from socket import *
+import time
 
-serverName = '192.168.226.1'  #此為當時寫這份作業時先用ipconfig來看我的主機ip
+serverName = 'localhost'  #此為當時寫這份作業時先用ipconfig來看我的主機ip
 serverPort = 10000
+
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 msg = input("input lowercase msg:\n")
 clientSocket.send(msg.encode())
+
 get_msg = clientSocket.recv(2048)
-print(get_msg)
+print(get_msg.decode())
 
 clientSocket.close()
+
+time.sleep(2)
