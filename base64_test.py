5a363154195f53c4d75eeb84c28040ef3fa6a2c1
hitler11319 <hitler11319@gmail.com>
2018-04-15 13:58:20 +0800

    base 64 test

--- base64_test.py
+++ base64_test.py
@@ -1,5 +1,5 @@
 import base64
-s1 = base64.b64encode(bytes('hello world', 'utf-8'))
-s2 = base64.b64decode(s1)
+s1 = base64.b64encode(bytes('hello world', 'utf-8')).decode()
+s2 = base64.b64decode(s1).decode()
 print (s1)
 print (s2)
